06217c3637f14af38572ac169f49d6297d53e2c6
Oliver Letterer <oliver.letterer@googlemail.com>
2012-02-11 11:08:35 +0100

    Sample Application.

---
+++ SampleApplication/SampleApplication/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SampleApplication
+//
+//  Created by Oliver Letterer on 11.02.12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
